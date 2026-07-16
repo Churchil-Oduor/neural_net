546713f8b1dd3b756479ebb5214b8a9832187b3d
Churchil-Oduor <churchilokechoduor@gmail.com>
2026-07-16 22:57:32 +0300

    NeuralNet class added

--- little_boy/engine.py
+++ little_boy/engine.py
@@ -1,65 +1,59 @@
 import numpy as np
 
-class Layer_Dense:
+class Layer:
 
     # initiating a layer
     def __init__(self, n_inputs, n_neurons):
-        self.n_inputs = n_inputs
-        self.n_neurons = n_neurons
-        self.biases = np.random.rand(n_neurons)
+        self.biases = np.zeros((1, n_neurons))
         self.weights = 0.1 * np.random.rand(n_inputs, n_neurons)
 
     # forward
     def forward(self, inputs):
         q = Activation_ReLU()
+        self.inputs = inputs
         self.output = q.forward(np.dot(inputs, self.weights) + self.biases)
-        
 
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis = 0, keepdims=True)
+
+        #Passing gradient to previous layer
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+
+# ReLU activation function
 class Activation_ReLU:
 
     # forward pass
     def forward(self, inputs):
-        return np.maximum(0, inputs)
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)
+        return self.output
         
+
+
+class NeuralNet:
+
+    def __init__(self):
+        self.layer1 = Layer(1, 1)
+        self.layer2 = Layer(1, 1)
+        self.layer3 = Layer(1, 1)
+        self.layer4 = Layer(1, 1)
+            
+
+    def forward(self, X):
+        self.layer1.forward(X)
+        self.layer2.forward(self.layer1.output)
+        self.layer3.forward(self.layer2.output)
+        self.layer4.forward(self.layer3.output)
+
+        return self.layer4.output
+
+
+    def backward(self, dloss):
+        self.layer4.backward(dloss)
+        self.layer3.backward(self.layer4.dinputs)
+        self.layer2.backward(self.layer3.dinputs)
+        self.layer1.backward(self.layer2.dinputs)
+        return self.layer1.dinputs
         
-class BackPropagation :
-
-    def __init__(self, l_rate, y_true, weights):
-        self.l_rate = l_rate
-        self.y_true = y_true
-        self.weights = weights
-
-
-    # computes the new weight using gradient decent
-    def new_weight(self, w, x):
-        self.weight = w
-        self.input = x
-        loss = self.weight * self.input - self.y_true
-        new_weight = self.weight - self.l_rate * 2 * loss * self.input
-        return new_weight
-
-
-    # handles back propagation
-    def propagate(self, inputs):
-       pass
-
-
-class Softmax:
-    pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- little_boy/main.py
+++ little_boy/main.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-from engine import Layer_Dense
+from engine import Layer
 from engine import Activation_ReLU
-from engine import BackPropagation
+from engine import NeuralNet 
 import numpy as np
 
-x = np.array([2])
-layer = Layer_Dense(1, 4)
-activation1 = Activation_ReLU()
-layer.forward(x)
-print(layer.output)
-rate = 0.1
-y_true = 10
-#b = BackPropagation(rate, y_true)
 
-#print(b.new_weight(4.4, 2))
+net = NeuralNet()
+X = np.array([[2]])
+
+prediction = net.forward(X)
+print(prediction)
+
 
