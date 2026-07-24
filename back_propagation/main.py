b39abac01bc1eec01d6903fdfe9a336ac590e1ca
Churchil-Oduor <churchilokechoduor@gmail.com>
2026-07-24 10:15:48 +0300

    updated the wq

--- back_propagation/main.py
+++ back_propagation/main.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python3
-from net import Layer
-from net import NeuralNet
+from net2 import Layer
+from net2 import NeuralNet
 import numpy as np
 
-l = Layer(1, 1)
-l2 = Layer(1, 1)
-l3 = Layer(1, 1)
-l4 = Layer(1, 1)
+np.random.seed(0)
 
-x_input = np.array([2])
 
-r = 0.01
-target = 10
-x_input = 2
+layers = [
+    Layer(1, 1),
+    Layer(1, 1),
+    Layer(1, 1),
+    Layer(1, 1)
+]
 
-net = NeuralNet(layers = [l, l2, l3, l4], r = r, x_input = x_input, target = target)
-predicted = net.forward()
+X = np.array([[3]])
+y = np.array([[100]])
 
-print(f"{predicted}")
+net = NeuralNet(layers, r=0.01)
 
-net.backpropagate()
-print("=============")
+for epoch in range(1000):
+    prediction = net.forward(X)
+    loss = net.compute_loss(y)
 
-net.backpropagate()
-net.backpropagate()
-net.backpropagate()
-net.backpropagate()
+    net.backpropagate()
 
-predicted = net.forward()
-
-print(f"{predicted}")
+    if epoch % 100 == 0:
+        print(epoch, loss)
 
 
--- back_propagation/net.py
+++ back_propagation/net.py
@@ -6,12 +6,18 @@ class Layer:
     def __init__(self, n_inputs, n_neurons):
         self.n_inputs = n_inputs
         self.n_neurons = n_neurons
-        self.weights = np.random.randn(n_inputs, n_neurons)
+
+        self.old_weights = np.random.randn(n_inputs, n_neurons)
+        self.new_weights = np.ones((n_inputs, n_neurons))
+        
+        self.f_weights = 1 # this will be update during backpropagation. represents the forward layer weights
+                                
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, x_input):
-        self.output = np.dot(x_input, self.weights) + self.biases
+        self.output = np.dot(x_input, self.old_weights) + self.biases
         return self.output
+
 
 class Activation_ReLU:
     def forward(self, fward_values):
@@ -19,11 +25,11 @@ class Activation_ReLU:
 
 class NeuralNet:
 
-    def __init__(self, layers = [], r=0.1, x_input = 0, target = 0):
+    def __init__(self, layers = [], r=0.1, x_input = 0, target = 0, y_pred = 0):
         self.layers = layers
         self.learning_rate = r
-        self.target = target
         self.x_input = x_input
+        self.loss = y_pred - target
 
     def forward(self):
 
@@ -38,20 +44,30 @@ class NeuralNet:
         return self.layers[last_index].output
 
 
-    def new_weight(self, w, y_pred):
-        loss = y_pred - self.target
-        gradient = 2 * loss * self.x_input
-        n_weight = w - self.learning_rate * gradient
-        return n_weight
+    def new_weight(self, old_w, grad):
+        new_weight = old_w - self.learning_rate * grad
+        return new_weight
+
+
+    def back_prop_action(self, c_layer, p_layer):
+        p_layer_output = p_layer.output
+        dl_dw = 2 * self.loss * c_layer.f_weights
+        p_layer.f_weights = dl_dw 
+        full_grad_decent = dl_dw * p_layer_output
+        c_layer.new_weights = self.new_weight(c_layer.old_weights, full_grad_decent)
+        weight_tray = c_layer.old_weights
+        c_layer.old_weights = c_layer.new_weights
+        c_layer.new_weights = weight_tray
 
     def backpropagate(self):
 
         for i in range(len(self.layers) - 1, 0, -1):
-            current = self.layers[i]
-            previous = self.layers[i - 1]
+            c_layer = self.layers[i] # current back layer
+            p_layer = self.layers[i - 1] # previous back layer
+            self.back_prop_action(c_layer, p_layer)
 
-            old_weight = previous.weights
-            y_pred = current.output
 
-            n_w = self.new_weight(old_weight, y_pred)
-            previous.weights = n_w
+    def view_weights(self):
+        for layer in self.layers:
+            print(layer.output)
+
